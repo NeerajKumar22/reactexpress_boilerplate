c38e0dd1affb6dcfbbc04ca6da2ce70269c85778
NeerajKumar22 <ineerajkr@gmail.com>
2019-08-20 17:05:27 +0530

    crapp as client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Customers from "./components/customers"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello App component....
+      </h1>
+      <Customers />
+    </div>
+  );
+}
+
+export default App;
